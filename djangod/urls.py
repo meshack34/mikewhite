92d658da146c6a7c6ad4f3f71a047ac0e4f4d238
meshack34 <meshackkimutai34@gmail.com>
2023-12-29 08:42:46 -0500

    first commit

--- djangod/urls.py
+++ djangod/urls.py
@@ -4,5 +4,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('credit/',include('django_daraja.urls'))
+    path('',include('django_daraja.urls'))
 ]
